82317ca1e9dd440e52d3f266e2e165734d8c57b7
Itacir Gabral <itacirgabral@gmail.com>
2020-06-19 12:54:51 -0400

    rotas

--- index.js
+++ index.js
@@ -1,1 +1,18 @@
+const express = require('express')
 const produtos = require('./produtos')
+
+const app = express()
+
+app.get('/', function (req, res) {
+  res.send('hello world')
+})
+
+app.get('/segundarota', function (req, res) {
+  res.send('segundarota')
+})
+
+app.get('/terceirarota', function (req, res) {
+  res.send('terceirarota')
+})
+
+app.listen(3000)
