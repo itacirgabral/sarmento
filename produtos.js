3a61208afe272c101f4bf58ca00a651797609a83
Itacir Gabral <itacirgabral@gmail.com>
2020-06-19 12:43:47 -0400

    produtos

---
+++ produtos.js
@@ -0,0 +1,7 @@
+const produtos = [
+  {"id": 1, "nome": "cafeteira", "quantidade": 30, "valorunitario": 500, "precototal": 750, "precodevenda": 300, "lucro": 250, "situacaodoproduto": "disponivel"},
+  {"id": 2, "nome": "fogão", "quantidade": 50, "valorunitario": 700, "precototal": 800, "precodevenda": 500, "lucro": 200, "situacaodoproduto": "indisponivel"},
+  {"id": 3, "nome": "lavadoura", "quantidade": 30, "valorunitario": 600, "precototal": 550, "precodevenda": 350, "lucro": 50, "situacaodoproduto": "disponivel"}
+]
+
+module.exports = produtos
